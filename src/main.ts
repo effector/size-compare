a0496ad8d804d00e713c8d39a3a1d2619adff07d
Sergey Sova <mail@sergeysova.com>
2022-10-05 22:13:12 +0400

    Check for gist files and do not log too much

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import {getInput, setFailed, setOutput} from '@actions/core';
 import github, {context, getOctokit} from '@actions/github';
 import {markdownTable} from 'markdown-table';
 
+const GIST_MAIN_FILE_NAME = 'main.json';
+const GIST_HISTORY_FILE_NAME = 'history.json';
+
 async function main() {
   const gistId = getInput('gist-id', {required: true});
   const token = getInput('token', {required: true});
@@ -47,18 +50,20 @@ async function main() {
 
   const gist = await octokit.rest.gists.get({gist_id: gistId});
   console.log('GIST', gist.data.files);
+  const mainFileExists = Boolean(gist.data.files?.[GIST_MAIN_FILE_NAME]);
+  const historyFileExists = Boolean(gist.data.files?.[GIST_HISTORY_FILE_NAME]);
 
   const time = new Date().toTimeString();
   setOutput('time', time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-  console.log(
-    markdownTable([
-      ['Branch', 'Commit'],
-      ['main', 'asdasda'],
-    ]),
-  );
+  // const payload = JSON.stringify(context.payload, undefined, 2);
+  // console.log(`The event payload: ${payload}`);
+  // console.log(
+  //   markdownTable([
+  //     ['Branch', 'Commit'],
+  //     ['main', 'asdasda'],
+  //   ]),
+  // );
 }
 
 main().catch((error) => {
